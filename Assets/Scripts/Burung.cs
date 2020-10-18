4a3c0388208bdd5447dc171f1db430f276cf8081
dzikrialifa <dzikrialif99@gmail.com>
2020-10-19 00:32:19 +0700

    tugas 7

---
+++ Assets/Scripts/Burung.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Burung : MonoBehaviour
+{
+    public void PindahBurung(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+
+}
